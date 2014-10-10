1b41738db346196a89d2511b3f63d869c9dba607
karyband <karyband@gmail.com>
2014-10-10 09:19:16 -0400

    signup php to check for accounts
    checking to see if there are duplicates of an email

--- signup.php
+++ signup.php
@@ -5,22 +5,37 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
+
 $firstname = mysqli_real_escape_string($connect,$_POST['first_name']);
 $lastname = mysqli_real_escape_string($connect, $_POST['last_name']);
 $myemail = mysqli_real_escape_string($connect, $_POST['my_email']);
 $myphone =mysqli_real_escape_string($connect, $_POST['my_phone']);
 
-$sql="INSERT INTO Users (firstName, lastName, email, phone)
-VALUES ('$firstname', '$lastname', '$myemail', '$myphone')";
 
-if (!mysqli_query($connect,$sql)) {
-  die('Error: ' . mysqli_error($connect));
+$query = "SELECT * FROM Users WHERE email = '$myemail'";
+$result = mysqli_query($connect, $query);
+
+$get_rows = mysqli_num_rows($result);
+if($get_rows < 1) {
+        $sql="INSERT INTO Users (firstName, lastName, email, phone)
+        VALUES ('$firstname', '$lastname', '$myemail', '$myphone')";
+
+        if (!mysqli_query($connect,$sql)) {
+                die('Error: ' . mysqli_error($connect));
+        }
+        echo "<center>";
+        echo "<h1>You have just signed up with Fooder!</h1>";
+        echo "<input type=button onClick=\"parent.location='../index.html'\" value='Continue'>";
+        echo "</center>";
+
+        mysqli_close($connect);
+
+} else {
+        echo "<center>";
+        echo "<h1>This email has already been used! Please use a different one.</h1>";
+        echo "<input type=button onClick=\"parent.location='../index.html'\" value='Continue'>";
+        echo "</center>";
+        
+   mysqli_close($connect);
 }
-
-echo "<center>";
-echo "<h1>You have just signed up with Fooder!</h1>";
-echo "<input type=button onClick=\"parent.location='home.html'\" value='Continue'>";
-echo "</center>";
-
-mysqli_close($connect);
 ?>
